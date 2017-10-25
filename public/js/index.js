4164e8bb7211a8e263b0706dde48869c53596f14
David Smart <David Smart>
2017-10-24 23:37:40 -0400

    Candace & David - set up preliminary UI

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var feed = new Vue({
+  el: '#event-feed',
+  data: {
+    events: [
+      { name: 'Lit', date: 'Oct. 31', time: '2pm', image: 'http://everyguyed.com/wp-content/uploads/2010/12/how-to-dress-for-a-night-out.jpg' },
+      { name: 'Boi', date: 'Oct. 31', time: '2pm' },
+      { name: 'Ayy', date: 'Oct. 31', time: '2pm' },
+      { name: 'Yah!', date: 'Oct. 31', time: '2pm' },
+      { name: 'Lit', date: 'Oct. 31', time: '2pm' },
+      { name: 'Boi', date: 'Oct. 31', time: '2pm' },
+      { name: 'Ayy', date: 'Oct. 31', time: '2pm' },
+      { name: 'Yah!', date: 'Oct. 31', time: '2pm' },
+      { name: 'Lit' , date: 'Oct. 31', time: '2pm'},
+      { name: 'Boi', date: 'Oct. 31', time: '2pm' },
+      { name: 'Ayy' , date: 'Oct. 31', time: '2pm'},
+      { name: 'Yah!', date: 'Oct. 31', time: '2pm'}
+    ]
+  }
+});
