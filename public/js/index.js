be1c8df878d40daff2e1d4cae55fe53beb0501df
David Smart <David Smart>
2017-10-26 00:01:34 -0400

    display event info

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,50 @@
-var feed = new Vue({
-  el: '#event-feed',
-  data: {
-    events: [
-      { name: 'Lit', date: 'Oct. 31', time: '2pm', image: 'http://everyguyed.com/wp-content/uploads/2010/12/how-to-dress-for-a-night-out.jpg' },
-      { name: 'Boi', date: 'Oct. 31', time: '2pm' },
-      { name: 'Ayy', date: 'Oct. 31', time: '2pm' },
-      { name: 'Yah!', date: 'Oct. 31', time: '2pm' },
-      { name: 'Lit', date: 'Oct. 31', time: '2pm' },
-      { name: 'Boi', date: 'Oct. 31', time: '2pm' },
-      { name: 'Ayy', date: 'Oct. 31', time: '2pm' },
-      { name: 'Yah!', date: 'Oct. 31', time: '2pm' },
-      { name: 'Lit' , date: 'Oct. 31', time: '2pm'},
-      { name: 'Boi', date: 'Oct. 31', time: '2pm' },
-      { name: 'Ayy' , date: 'Oct. 31', time: '2pm'},
-      { name: 'Yah!', date: 'Oct. 31', time: '2pm'}
-    ]
-  }
-});
+var feed;
+
+function getEvents(){
+  var result = [1,2,3];
+
+  $.ajax({
+    url: '/search',
+    data: {},
+    success: function(response){
+       render(response);
+      },
+   });
+}
+
+function render(ev){
+  feed = new Vue({
+    el: '#event-feed',
+    data: {
+      events: ev
+    }
+  });
+}
+
+getEvents();
+
+// Get the modal
+var modal = document.getElementById('event-Modal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
