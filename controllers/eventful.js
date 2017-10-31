eb9631d6a8b532e5aaa62386f11c8ab0e938ec79
Joshua Bartlett <jcbart12595@gmail.com>
2017-10-30 23:17:55 -0400

    reorganized modules in the backend and added more test cases

--- controllers/eventful.js
+++ controllers/eventful.js
@@ -1,7 +1,9 @@
+var exports = module.exports = {};
 var eventful = require('eventful-node');
 var client = new eventful.Client('BdCFq6HP79LPJq8B');
 
-function Event(title, desc, img, url, date, venue) {
+// Event class
+var Event = function(id, title, desc, img, url, date, venue) {
 
     this.id = id;
     this.title = title;
@@ -12,25 +14,57 @@ function Event(title, desc, img, url, date, venue) {
     this.venue = venue;
 }
 
-function search(where, callback) {
+// Removes the duplicate events from the event list
+var removeDuplicates = function(events) {
 
-    client.searchEvents({page_size: 100, location: where}, function(err, data){
+    var seen = new Set();
+    var new_events = [];
+
+    for(var i in events) {
+        current_event = events[i];
+
+        // Only add unique titles
+        if (!seen.has(current_event.title)) {
+            seen.add(current_event.title);
+            new_events.push(current_event);
+        }
+    }
+
+    return new_events;
+}
+
+// Searches eventful for 35 random events 
+exports.search = function(where, callback) {
+
+    client.searchEvents({page_size: 35, location: where}, function(err, data){
 
         if(err){
             console.log(err);
-            return null;
+            callback(null);
         }
 
+        events = [];
+
         console.log('Recieved ' + data.search.total_items + ' events');
-        console.log('Event listings: ');
           
         //print the title of each event 
         for(var i in data.search.events.event){
-          
-            console.log(data.search.events.event[i].title);
-          
+
+            var current_event = data.search.events.event[i];
+            var id = current_event.$.id;
+            var title = current_event.title;
+            var desc = current_event.description;
+            var img = current_event.image.url;
+            var date = current_event.start_time;
+            var venue = current_event.venue_name;
+            var url = current_event.url;
+
+            events.push(new Event(id, title, desc, img, url, date, venue));         
         }
+
+        callback(removeDuplicates(events));
     });
 }
 
-module.exports = Event;
+exports.Event = Event;
+exports.removeDuplicates = removeDuplicates;
