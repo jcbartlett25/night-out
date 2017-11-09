1684e0a7599b518d12029d2d86acd1614203c0b4
Candace Johnson <crj2121@columbia.edu>
2017-11-08 23:33:24 -0500

    added tests for our event class

--- test/test.js
+++ test/test.js
@@ -1,21 +1,6 @@
 var assert = require('assert');
 var eventful = require('../controllers/eventful');
 
-describe('Array', function() {
-
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-
-  describe('#indexOf()', function() {
-    it('should return the correct index of the value in the array', function() {
-        assert.equal(1, [1,2,3].indexOf(2));
-    });
-  });
-
-});
 
 describe('Eventful', function() {
 
@@ -26,7 +11,14 @@ describe('Eventful', function() {
         done();
       });
     });
+    it('should return no events when specified location doesnt exist', function(){
+      eventful.search('Canda', function(events) {
+        assert.equal(0, events.length);
+        done();
+      });
+    });
   });
+
 
   describe('#removeEvents()', function() {
     it('should remove all duplicate titles from a list of events', function() {
@@ -37,6 +29,18 @@ describe('Eventful', function() {
       events = eventful.removeDuplicates(events);
       assert.equal(2, events.length);
     });
+    it('should remove all duplicate titles from an empty list of events', function() {
+      var events = [];
+      events = eventful.removeDuplicates(events);
+      assert.equal(0, events.length);
+    });
+    it('should remove all duplicate titles from a list of 1 event', function() {
+      var event1 = new eventful.Event(null, "title");
+      var events = [event1];
+      events = eventful.removeDuplicates(events);
+      assert.equal(1, events.length);
+    });
   });
+
 
 });
