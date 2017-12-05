f9f05a9925b770554852a5309785a4dc419364c8
David Smart <David Smart>
2017-12-05 12:15:10 -0500

    started working on event page

---
+++ public/js/my-events.js
@@ -0,0 +1,26 @@
+//get events using Ajax API call to Josh's code
+function getMyEvents(){//TODO
+    $('#event-feed').hide();
+  
+    $.ajax({
+      url: '/search',
+      data: {
+        where: 'New York'
+      },
+      success: function(response){
+        $('#event-feed').show();
+         renderFeed(response);
+        },
+     });
+  }
+
+//create view object for event feed
+function renderFeed(ev){
+    feed_vue = new Vue({
+      el: '#event-feed', 
+      data: {
+        events: ev
+      }
+    });
+  }
+  
